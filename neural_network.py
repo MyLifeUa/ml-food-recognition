63540f33dbe0ec38f15eeee115ba8f3c2ccbbcf8
joao-p-marques <joao.marques.contacto@gmail.com>
2020-03-26 15:26:22 +0000

    build CNN and model

--- neural_network.py
+++ neural_network.py
@@ -1,13 +1,84 @@
 
 import tensorflow as tf
 
-from tensorflow.keras.prepocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import shutil
+import collections
 
 print('imports success')
 
-base_dir = '/home/jota/Documents/UA/PI/ML/cnn_keras/food-101/'
+print('Spliting data between test and train dirs')
+
+# Only split files if haven't already
+if not os.path.isdir('./food-101/test') and not os.path.isdir('./food-101/train'):
+
+    def copytree(src, dst, symlinks = False, ignore = None):
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+            shutil.copystat(src, dst)
+        lst = os.listdir(src)
+        if ignore:
+            excl = ignore(src, lst)
+            lst = [x for x in lst if x not in excl]
+        for item in lst:
+            s = os.path.join(src, item)
+            d = os.path.join(dst, item)
+            if symlinks and os.path.islink(s):
+                if os.path.lexists(d):
+                    os.remove(d)
+                os.symlink(os.readlink(s), d)
+                try:
+                    st = os.lstat(s)
+                    mode = stat.S_IMODE(st.st_mode)
+                    os.lchmod(d, mode)
+                except:
+                    pass # lchmod not available
+            elif os.path.isdir(s):
+                copytree(s, d, symlinks, ignore)
+            else:
+                shutil.copy2(s, d)
+
+    def generate_dir_file_map(path):
+        dir_files = collections.defaultdict(list)
+        with open(path, 'r') as txt:
+            files = [l.strip() for l in txt.readlines()]
+            for f in files:
+                dir_name, id = f.split('/')
+                dir_files[dir_name].append(id + '.jpg')
+        return dir_files
+
+    train_dir_files = generate_dir_file_map('food-101/meta/train.txt')
+    test_dir_files = generate_dir_file_map('food-101/meta/test.txt')
+
+
+    def ignore_train(d, filenames):
+        print(d)
+        subdir = d.split('/')[-1]
+        to_ignore = train_dir_files[subdir]
+        return to_ignore
+
+    def ignore_test(d, filenames):
+        print(d)
+        subdir = d.split('/')[-1]
+        to_ignore = test_dir_files[subdir]
+        return to_ignore
+
+    copytree('food-101/images', 'food-101/test', ignore=ignore_train)
+    copytree('food-101/images', 'food-101/train', ignore=ignore_test)
+
+else:
+    print('Train/Test files already copied into separate folders.')
+
+print('done')
+
+base_dir = './food-101/'
 
 train_dir = os.path.join(base_dir, 'train')
 test_dir = os.path.join(base_dir, 'test')
@@ -15,8 +86,8 @@ test_dir = os.path.join(base_dir, 'test')
 # each class will be inside here
 
 # all images will be rescaled by 1./255
-train_datagen = ImageDatGenerator(rescale=1.0/255.)
-test_datagen = ImageDatGenerator(rescale=1.0/255.)
+train_datagen = ImageDataGenerator(rescale=1.0/255.)
+test_datagen = ImageDataGenerator(rescale=1.0/255.)
 
 # flow train images in batches of 20 using train_datagen
 train_generator = train_datagen.flow_from_directory(train_dir,
